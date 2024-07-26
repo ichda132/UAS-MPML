ae1ecc01161a131a9090002af163cf4d6c7424a2
ichda132 <22611132@students.uii.ac.id>
2024-07-26 21:39:20 +0700

    Update streamlit-uber.py

--- streamlit-uber.py
+++ streamlit-uber.py
@@ -3,7 +3,7 @@ import joblib
 import numpy as np
 
 # Muat model dan scaler yang sudah disimpan
-model = joblib.load('random_forest_model_compressed2.sav')
+model = joblib.load('random_forest_model.sav')
 scaler = joblib.load('scaler.sav')
 
 # Fungsi untuk melakukan prediksi
@@ -15,10 +15,10 @@ def predict(features):
 
 # Streamlit UI
 st.title('Prediksi Tarif Uber')
-st.write('Masukkan fitur untuk prediksi tarif Uber')
+st.write('Masukkan fitur untuk prediksi tarif uber')
 
 # Input fitur dari pengguna
-distance = st.number_input('Distance')
+distance = st.number_input('Distance (km)')
 passenger_count = st.number_input('Passenger Count', min_value=1, max_value=10, step=1)
 day = st.number_input('Day', min_value=1, max_value=31, step=1)
 month = st.number_input('Month', min_value=1, max_value=12, step=1)
@@ -28,6 +28,6 @@ dayofweek = st.number_input('Day of Week', min_value=0, max_value=6, step=1)
 
 # Melakukan prediksi saat tombol ditekan
 if st.button('Predict'):
-    features = [distance, passenger_count, day, month, year, hour, dayofweek]
+    features = [passenger_count, hour, day, month, year, dayofweek, distance]
     prediction = predict(features)
     st.write(f'Tarif Prediksi: ${prediction[0]:.2f}')
